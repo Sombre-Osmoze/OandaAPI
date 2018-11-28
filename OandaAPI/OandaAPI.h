fa0198befdc2a209105117b684f34eca76a2742d
Marcus Florentin <fmarcus28@gmx.fr>
2018-11-28 01:05:33 +0100

    Initial Commit

---
+++ OandaAPI/OandaAPI.h
@@ -0,0 +1,19 @@
+//
+//  OandaAPI.h
+//  OandaAPI
+//
+//  Created by Marcus Florentin on 28/11/2018.
+//  Copyright © 2018 Marcus Florentin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OandaAPI.
+FOUNDATION_EXPORT double OandaAPIVersionNumber;
+
+//! Project version string for OandaAPI.
+FOUNDATION_EXPORT const unsigned char OandaAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OandaAPI/PublicHeader.h>
+
+
