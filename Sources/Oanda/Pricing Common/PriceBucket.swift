0cb5d0b559cf5cee9870c36c75eac72e08478aab
@Sombre-Osmoze <sombre@osmoze.xyz>
2022-07-23 19:53:57 +0200

    Adding "Pricing Common"

---
+++ Sources/Oanda/Pricing Common/PriceBucket.swift
@@ -0,0 +1,18 @@
+//
+//  PriceBucket.swift
+//  Pricing Common
+//
+//  Created by @Sombre-Osmoze on 23/07/2022.
+//
+
+import Foundation
+
+/// A PriceBucket represents a price available for an amount of liquidity.
+public struct PriceBucket<Liquidity: SignedNumeric & Codable>: Codable {
+
+				/// The Price offered by the PriceBucket.
+				public let price: PriceValue
+
+				/// The amount of liquidity offered by the PriceBucket
+				public let liquidity: Liquidity
+}
---
+++ Sources/Oanda/Pricing Common/PriceValue.swift
@@ -0,0 +1,13 @@
+//
+//  PriceValue.swift
+//  Pricing Common
+//
+//  Created by @Sombre-Osmoze on 23/07/2022.
+//
+
+import Foundation
+
+/// The string representation of a Price for a Bucket.
+///  A decimal number encodes as a string.
+///  - note: The amount of precision provided depends on the Instrument.
+public typealias PriceValue = String
