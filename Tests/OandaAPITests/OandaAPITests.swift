69b2f7e035f39d29c9c7f33ef1abeff9fbebda3d
@Sombre-Osmoze <sombre@osmoze.xyz>
2022-12-18 08:13:53 +0000

    Updating old test folder

--- Tests/OandaAPITests/OandaAPITests.swift
+++ Tests/OandaAPITests/OandaAPITests.swift
@@ -5,4 +5,14 @@
 //  Created by sombre@osmoze.xyz on 18/12/2022.
 //
 
+import XCTest
 import Foundation
+@testable import OandaAPI
+import Mocker
+
+class OandaAPITests: APITestCase {
+
+				
+}
+
+
